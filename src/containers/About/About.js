4f92600762a958638237e33d0ef0bb86e21a5afe
Daniel Oh <doh422@gmail.com>
2016-02-03 15:34:53 -0800

    personality traits and types widgets displayed

--- src/containers/About/About.js
+++ src/containers/About/About.js
@@ -33,23 +33,23 @@ export default class About extends Component {
     saving: PropTypes.bool,
     results: PropTypes.object,
     // user props
-    user: PropTypes.object,
+    user: PropTypes.object
   }
 
   // check to see if test has been taken
-  componentWillMount() {
-    const {user} = this.props;
-    console.log(user);
-    if (!user) {
-      alert('please login');
-    } else if (user && !user.test_id) {
-      alert('please take assessment');
-    } else if (user.test_id && !user.results.complete) {
-      alert('please finish assessment');
-    } else if (user.test_id && user.results.complete) {
-      alert('congrats');
-    }
-  }
+  // componentWillMount() {
+  //   const {user} = this.props;
+  //   console.log(user);
+  //   if (!user) {
+  //     alert('please login');
+  //   } else if (user && !user.test_id) {
+  //     alert('please take assessment');
+  //   } else if (user.test_id && !user.results.complete) {
+  //     alert('please finish assessment');
+  //   } else if (user.test_id && user.results.complete) {
+  //     alert('congrats');
+  //   }
+  // }
 
   componentDidMount() {
     // http://facebook.github.io/react/docs/component-specs.html
@@ -78,7 +78,9 @@ export default class About extends Component {
       });
     }
     if (user.results.complete) {
-      Traitify.ui.load('results', assessment.id, '.result-deck');
+      Traitify.ui.load('results', user.test_id, '.result-deck');
+      Traitify.ui.load('personalityTypes', user.test_id, '.personality-types');
+      Traitify.ui.load('personalityTraits', user.test_id, '.personality-traits');
     }
   }
 
@@ -99,25 +101,29 @@ export default class About extends Component {
         alert('finished test');
         // results retrieved and saved to store
         getPersonalityTypes(String(assessment.id));
-        user.test_id = assessment.id;
-        console.log(assessment);
+        // console.log(assessment);
+        // console.log(user);
+        // console.log(results);
+        user.results.complete = true;
+
       });
     }
     if (results && results.personality_blend.name) {
     // show results
-      const showResults = Traitify.ui.load('results', assessment.id, '.result-deck');
+      const showResults = Traitify.ui.load('results', user.test_id, '.result-deck');
+      Traitify.ui.load('personalityTypes', user.test_id, '.personality-types');
+      Traitify.ui.load('personalityTraits', user.test_id, '.personality-traits');
       showResults.onInitialize(function() {
-        user.results.complete = true;
         user.results.personalities = results.personality_blend;
         user.results.personality_types = results.personality_types;
-        console.log(user);
-        console.log(results);
+        // console.log(user);
+        // console.log(results);
       });
     }
   }
 
   render() {
-    const {assessment, createAssessment, createError, creating, saveError, saving} = this.props;
+    const {assessment, createAssessment, createError, creating, saveError, saving, user} = this.props;
     return (
       <div className="container">
         <h1>Traitify Assessment</h1>
@@ -132,6 +138,8 @@ export default class About extends Component {
         {createError && <div>{JSON.stringify(createError)}</div>}
         {assessment && <div className="slide-deck"/>}
         <div className="result-deck"></div>
+        <div className="personality-types"></div>
+        <div className="personality-traits"></div>
         {user.results.complete && <button>Retake Assessment</button>}
         {saving && <div>Saving...</div>}
         {saveError && <div>{JSON.stringify(saveError)}</div>}
